d11fbc293f63d3d6bfcdc134c55d59f1079e480c
NgomsoGaya <ngomso.gaya09@gmail.com>
2023-11-09 11:37:55 +0200

    fixed filter by size and color

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.get("/admin", mainrender.admin)
 app.get("/api/shoes", mainrender.getAllAPIShoes);
 app.get("/api/shoes/brand/:brandname/color/:shoecolor/size/:shoesize", mainrender.filterByBrandColorSizeAPI)
 app.get("/api/shoes/brand/:brandname/color/:shoecolor", mainrender.filterByBrandColorAPI)
-app.get("/api/shoes/color/:shoecolor/size/:shoesize", mainrender.filterByBrandColorSizeAPI)
+app.get("/api/shoes/color/:shoecolor/size/:shoesize", mainrender.filterByColorSizeAPI)
 app.get("/api/shoes/brand/:brandname/size/:shoesize", mainrender.filterByBrandSizeAPI)
 app.get("/api/shoes/brand/:brandname", mainrender.filterByBrandAPI)
 app.get("/api/shoes/size/:shoesize", mainrender.filterBySizeAPI)
